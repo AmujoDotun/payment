55a5a31a4ac204899cdec2f260bb9b87e0fccffa
AmujoDotun <samuelamujo@gmail.com>
2020-02-14 04:30:25 +0100

    created and function for product and review and assign model relationship

--- app/Review.php
+++ app/Review.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App;
+use App\Product;s
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+    
 }
