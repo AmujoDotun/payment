9de1234913de6df234032341b560d28d2225e091
AmujoDotun <samuelamujo@gmail.com>
2020-02-15 15:08:00 +0100

    makes correction to the foreign key, add s to reference and make the casecade capotal letters

--- database/migrations/2020_02_14_024507_create_reviews_table.php
+++ database/migrations/2020_02_14_024507_create_reviews_table.php
@@ -16,7 +16,7 @@ class CreateReviewsTable extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('product_id')->unsigned()->index();
-            $table->foreign('product_id')->reference('id')->on('products')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
             $table->string('customer');
             $table->text('review');
             $table->integer('star');
